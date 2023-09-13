a7df6c7e280a2c6c67650ae0b8df727faeb784b7
Farida Andog <144873900+fhar1225@users.noreply.github.com>
2023-09-13 14:28:34 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+    static void DisplayAccountDetails(Account account1){
+        Console.WriteLine($"Account Number: {account1.AccountNumber}");
+        Console.WriteLine($"Balance: ${account1.Balance}");
+    }
+    static void Main(string [] args){
+        SavingsAccount savingsAccount = new SavingsAccount(12345, 12.0);
+        CheckingAccount checkingAccount = new CheckingAccount(1025,12.0);
+
+        savingsAccount.Deposit(10000);
+        DisplayAccountDetails(savingsAccount);
+        savingsAccount.CalculateInterest();
+        DisplayAccountDetails(savingsAccount);
+
+        Console.WriteLine();
+
+        checkingAccount.Deposit(100);
+        DisplayAccountDetails(checkingAccount);
+        checkingAccount.Withdraw(600);
+        DisplayAccountDetails(checkingAccount);
+    }
+}
